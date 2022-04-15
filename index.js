5c75d190fa0e67a6456363996fecdc84bd73e719
Your Name <you@example.com>
2022-04-15 12:52:07 +0300

    initial commit

--- index.js
+++ index.js
@@ -87,3 +87,19 @@ app.listen(PORT, () => {
 const cors = require('cors')
 
 app.use(cors())
+
+const logger = (request, response, next) => {
+    console.log('Method:',request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+  const error = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+  }
+
+
+app.use(express.static('build'))
+app.use(error)
+app.use(logger)
